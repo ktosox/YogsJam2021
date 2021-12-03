3c6dfffb8155f4ddaa9edb7e4f8754ac13922a0c
Ktosox <ktoso3@gmail.com>
2021-12-03 22:00:38 +0100

    adding player

---
+++ Game/Actors/Player/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	direction.x = int(Input.is_action_pressed("ui_right") ) - int(Input.is_action_pressed("ui_left") )
+	direction.y = int(Input.is_action_pressed("ui_down") ) - int(Input.is_action_pressed("ui_up") )
+	direction = direction.normalized()
+	
+	move_and_collide(direction*20*delta,false)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
